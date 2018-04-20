2f9a999464f404a107a9605ee87a791cb3f794e0
JustinChowBeliefChain <zhouzhiyu@beliefchain.com>
2018-04-20 23:47:22 +0800

    add more details

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const bip39 = require("bip39")
 const hdkey = require('ethereumjs-wallet/hdkey')
 
 function generate(mnemonic) {
-  const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic))
-  const wallet_hdpath = "m/44'/60'/0'/0/"
+    const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic))
+    const wallet_hdpath = "m/44'/60'/0'/0/"
 
-  const wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet()
-  return {
-    priv: wallet._privKey.toString('hex'),
-    pub: '0x' + wallet.getAddress().toString('hex')
-  }
+    const wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet()
+    return {
+        mnemonic: mnemonic,
+        priv: wallet._privKey.toString('hex'),
+        account: '0x' + (wallet.getAddress().toString('hex').toUpperCase())
+    }
 }
 
 module.exports = generate
