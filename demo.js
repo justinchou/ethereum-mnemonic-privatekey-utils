7159f08bcbf52e3a61cb709729a53a32c1df63ac
JustinChowBeliefChain <zhouzhiyu@beliefchain.com>
2018-07-02 21:00:49 +0800

    add 0x as prefix of address, format code

--- demo.js
+++ demo.js
@@ -5,17 +5,17 @@ pkutils.debug = false;
 const mnemonic = 'yesterday once more happy bride smile short lovers make life sound great';
 const password = 'this is a totally long password';
 
-console.log('mnemonic:         %s', mnemonic);
-console.log('password:         %s', password);
+console.log('mnemonic               : %s', mnemonic);
+console.log('password               : %s', password);
 
 const privateKeyGen = pkutils.getPrivateKeyFromMnemonic(mnemonic);
-console.log('private key:      %s', privateKeyGen);
+console.log('pkey from mnemonic     : 0x%s', privateKeyGen);
 
 const keystore = pkutils.getKeystoreFromPrivateKey(privateKeyGen, password);
-console.log('key store:        %j', keystore);
+console.log('\nkey store              : %j\n', keystore);
 
 const privateKeyParsed = pkutils.getPrivateKeyFromKeystore(keystore, password);
-console.log('private key:      %s', privateKeyParsed);
+console.log('pkey from keystore     : 0x%s', privateKeyParsed);
 
 const account = keystore.address;
-console.log('account address:  %s', account);
+console.log('account address        : 0x%s', account);
--- index.js
+++ index.js
@@ -1,83 +1,89 @@
-const bip39 = require("bip39");
+const bip39 = require('bip39');
 const hdkey = require('ethereumjs-wallet/hdkey');
-const keythereum = require("keythereum");
+const keythereum = require('keythereum');
 
 exports.debug = false;
 
 function getPrivateKeyFromMnemonic(mnemonic) {
-    const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
-    const wallet_hdpath = "m/44'/60'/0'/0/";
+	const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+	const wallet_hdpath = "m/44'/60'/0'/0/";
 
-    const wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet();
-    const privateKey = wallet._privKey.toString('hex');
-    const account = '0x' + (wallet.getAddress().toString('hex').toUpperCase());
+	const wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet();
+	const privateKey = wallet._privKey.toString('hex');
+	const account = '0x' + wallet.getAddress().toString('hex').toUpperCase();
 
-    if (exports.debug) console.log({
-        mnemonic: mnemonic,
-        privateKey: privateKey,
-        account: account
-    });
+	if (exports.debug)
+		console.log({
+			mnemonic: mnemonic,
+			privateKey: privateKey,
+			account: account
+		});
 
-    return privateKey
+	return privateKey;
 }
 
 exports.getPrivateKeyFromMnemonic = getPrivateKeyFromMnemonic;
 
 function getPrivateKeyFromRandom() {
-    const params = {keyBytes: 32, ivBytes: 16};
-    const dk = keythereum.create(params);
+	const params = { keyBytes: 32, ivBytes: 16 };
+	const dk = keythereum.create(params);
 
-    const privateKey = dk.privateKey.toString('hex');
+	const privateKey = dk.privateKey.toString('hex');
 
-    if (exports.debug) console.log({
-        privateKey: privateKey,
-        salt: dk.salt.toString('hex'),
-        iv: dk.iv.toString('hex')
-    });
+	if (exports.debug)
+		console.log({
+			privateKey: privateKey,
+			salt: dk.salt.toString('hex'),
+			iv: dk.iv.toString('hex')
+		});
 
-    return privateKey
+	return privateKey;
 }
 
 exports.getPrivateKeyFromRandom = getPrivateKeyFromRandom;
 
 function getPrivateKeyFromKeystore(keystore, password) {
-    const privateKeyBuf = keythereum.recover(password, keystore);
-    const privateKey = privateKeyBuf.toString('hex')
+	const privateKeyBuf = keythereum.recover(password, keystore);
+	const privateKey = privateKeyBuf.toString('hex');
 
-    if (exports.debug) console.log({
-        privateKey: privateKey,
-        password: password,
-        keystore: keystore
-    });
+	if (exports.debug)
+		console.log({
+			privateKey: privateKey,
+			password: password,
+			keystore: keystore
+		});
 
-    return privateKey
+	return privateKey;
 }
 
 exports.getPrivateKeyFromKeystore = getPrivateKeyFromKeystore;
 
 function getKeystoreFromPrivateKey(privateKey, password) {
-    const params = {keyBytes: 32, ivBytes: 16};
-    const dk = keythereum.create(params);
+	const params = { keyBytes: 32, ivBytes: 16 };
+	const dk = keythereum.create(params);
 
-    const options = {
-        kdf: "pbkdf2",
-        cipher: "aes-128-ctr",
-        kdfparams: {
-            c: 262144,
-            dklen: 32,
-            prf: "hmac-sha256"
-        }
-    };
-    // const keystore = keythereum.dump(password, dk.privateKey, dk.salt, dk.iv, options);
-    const keystore = keythereum.dump(password, privateKey, dk.salt, dk.iv, options);
+	const options = {
+		kdf: 'pbkdf2',
+		cipher: 'aes-128-ctr',
+		kdfparams: {
+			c: 262144,
+			dklen: 32,
+			prf: 'hmac-sha256'
+		}
+	};
 
-    if (exports.debug) console.log({
-        privateKey: privateKey,
-        password: password,
-        keystore: keystore
-    });
+	// const keystore = keythereum.dump(password, dk.privateKey, dk.salt, dk.iv, options);
+	const keystore = keythereum.dump(password, privateKey, dk.salt, dk.iv, options);
 
-    return keystore;
+	if (exports.debug) {
+		console.log({
+			privateKey: privateKey,
+			password: password,
+			keystore: keystore
+		});
+	}
+
+	return keystore;
 }
 
 exports.getKeystoreFromPrivateKey = getKeystoreFromPrivateKey;
